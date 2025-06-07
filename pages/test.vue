af8e73e84b9b253aaba0b344233bb87469e5d8bd
wermoss <konrad@minister.com>
2025-06-07 08:31:01 +0200

    Xminana - podsumowanie

---
+++ pages/test.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="w-full border-1 border-amber-300">aaa</div>
+  <p>container cols-12 gap-4</p>
+  <div class="container mx-auto text-center">
+    <div class="grid grid-cols-12 gap-4">
+      <div class="bg-gray-300">01</div>
+      <div class="bg-gray-300">02</div>
+      <div class="bg-gray-300">03</div>
+      <div class="bg-gray-300">04</div>
+      <div class="bg-gray-300">05</div>
+      <div class="bg-gray-300">06</div>
+      <div class="bg-gray-300">07</div>
+      <div class="bg-gray-300">08</div>
+      <div class="bg-gray-300">09</div>
+      <div class="bg-gray-300">10</div>
+      <div class="bg-gray-300">11</div>
+      <div class="bg-gray-300">12</div>
+    </div>
+  </div>
+  <p>container cols-12 gap-4</p>
+  <div class="container mx-auto text-center max-w-7xl">
+    <div class="grid grid-cols-12 gap-4">
+      <div class="bg-gray-300">01</div>
+      <div class="bg-gray-300">02</div>
+      <div class="bg-gray-300">03</div>
+      <div class="bg-gray-300">04</div>
+      <div class="bg-gray-300">05</div>
+      <div class="bg-gray-300">06</div>
+      <div class="bg-gray-300">07</div>
+      <div class="bg-gray-300">08</div>
+      <div class="bg-gray-300">09</div>
+      <div class="bg-gray-300">10</div>
+      <div class="bg-gray-300">11</div>
+      <div class="bg-gray-300">12</div>
+    </div>
+  </div>
+  <p>container cols-12 gap-4</p>
+  <div class="container mx-auto text-center max-w-6xl">
+    <div class="grid grid-cols-12 gap-4">
+      <div class="bg-gray-300">01</div>
+      <div class="bg-gray-300">02</div>
+      <div class="bg-gray-300">03</div>
+      <div class="bg-gray-300">04</div>
+      <div class="bg-gray-300">05</div>
+      <div class="bg-gray-300">06</div>
+      <div class="bg-gray-300">07</div>
+      <div class="bg-gray-300">08</div>
+      <div class="bg-gray-300">09</div>
+      <div class="bg-gray-300">10</div>
+      <div class="bg-gray-300">11</div>
+      <div class="bg-gray-300">12</div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
